57c84c6fe9370076ce147b156ab899c3df6e23eb
blackdogcode <blackdogcode@gmail.com>
2021-11-23 12:27:08 +0900

    for loop multiple table example

---
+++ BashScript/if_statement/multiple_table.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+for i in {1..9}
+do
+	for ((j = 1; j < 10; j++))
+	do
+		((result = i * j))
+		if [ "$result" -lt 30 ]; then
+			echo "$i x $j = $result"
+		else
+			echo "$i x $j = give up"
+			break
+		fi
+	done
+	echo ""
+	
+done
